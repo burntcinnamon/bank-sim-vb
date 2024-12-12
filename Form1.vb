e5edf1b1923f525269cb64370626060c3b3965fa
burntcinnamon <107076321+burntcinnamon@users.noreply.github.com>
2024-12-12 22:16:29 +0000

    add code

---
+++ Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Structure Account
+    Dim usrName As String
+    Dim pin As String
+    Dim money As Single
+End Structure
+
+Public Class Form1
+    Public a As Account
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Call SignUp()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Call Login()
+    End Sub
+
+    Sub Login()
+        Dim namePut As String
+        Dim passPut As String
+
+        namePut = InputBox("What's your username?")
+        passPut = InputBox("What's your password?")
+
+
+        If a.usrName = namePut And a.pin = passPut Then
+            MsgBox("Authentication Successful.")
+            MainPage.Show()
+            Me.Hide()
+        Else
+            MsgBox("Your username or PIN was incorrect. Try again.")
+        End If
+    End Sub
+
+    Sub SignUp()
+        Dim name As String
+        Dim pass As Integer
+
+
+        MsgBox("Thanks for choosing XYZ Bank! Before you start your exquisite financial journey, we need some information from you.", MsgBoxStyle.Information, "Signup")
+
+        name = InputBox("What's your username?", "Signup")
+        a.usrName = name
+        pass = InputBox("Cool " & name & "! Now, what's your PIN? Keep it so hard to guess that hackers won't have an easy chance of taking your money, but easy enough for you to remember! 5 characters is the limit.", "Signup")
+
+        If pass.ToString().Length >= 5 Then
+            MsgBox("Error! You have entered a PIN that's too big. Try Again.")
+            pass = InputBox("Cool " & name & "! Now, what's your PIN? Keep it so hard to guess that hackers won't have an easy chance of taking your money, but easy enough for you to remember! 5 characters is the limit.", "Signup")
+        Else
+            a.pin = pass
+        End If
+
+        MsgBox("You have now completed the sign up process for XYZ Bank - simple, right? - We look forward to banking with you.")
+    End Sub
+End Class
---
+++ MainPage.vb
@@ -0,0 +1,46 @@
+﻿Public Class MainPage
+    Private Sub MainPage_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        HiText.Text = "Hi " + Form1.a.usrName + "!"
+
+        moneyTxt.Text = "£" & Form1.a.money
+    End Sub
+
+    Private Sub depositBtn_Click(sender As Object, e As EventArgs) Handles depositBtn.Click
+        Dim moneyInput As Single ' input of deposit
+
+        moneyInput = InputBox("How much money do you want to deposit?")
+
+        Call DepositMoney(moneyInput)
+    End Sub
+
+    Sub DepositMoney(money As Single)
+        Form1.a.money += money
+
+        moneyTxt.Text = "£" & Form1.a.money
+    End Sub
+
+    Private Sub withdrawBtn_Click(sender As Object, e As EventArgs) Handles withdrawBtn.Click
+        Dim moneyInput As Single ' withdraw amount
+
+        moneyInput = InputBox("How much do you want to widthdraw from your account?")
+
+        WithdrawMoney(moneyInput)
+    End Sub
+
+    Sub WithdrawMoney(money As Single)
+        Form1.a.money -= money
+
+        moneyTxt.Text = "£" & Form1.a.money
+    End Sub
+
+    Private Sub logOutBtn_Click(sender As Object, e As EventArgs) Handles logOutBtn.Click
+        Call logOut()
+    End Sub
+
+    Sub logOut()
+        MsgBox("Goodbye, we hope you come back soon!", , "Goodbye!")
+
+        Me.Hide()
+        Form1.Show()
+    End Sub
+End Class
